3efdb3e8c609f90acb9c144a6e6bb39538015909
mdaabidali11200 <mdaabidali11200@gmail.com>
2024-01-30 18:59:46 +0530

    first commit

---
+++ script3.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $("#hide").click(function () {
+      $("#lorem").hide(1000);
+    });
+    $("#show").click(function () {
+      $("#lorem").show(1000);
+    });
+  });
+  $(document).ready(function () {
+    $("#p").click(function () {
+      $(this).hide(4000);
+    });
+  });
